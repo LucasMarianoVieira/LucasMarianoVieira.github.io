09f0fb507817cc86545a12e97708b7d7fbd12189
Lucas Mariano Vieira <lucasmarianovieira@gmail.com>
2017-07-25 13:42:01 -0300

    commit 3

--- a.js
+++ a.js
@@ -1,6 +1,6 @@
 var context= new window.AudioContext();
 var index=0;
-var n=4096;
+var n=512;
 
 var reader;
 var input;
@@ -8,6 +8,8 @@ var buf;
 var audioBuffer;
 var saida=new Array();
 var samples;
+var lev=new Array();
+var mags;
 
 function play(buffer){
 	var source =context.createBufferSource();
@@ -41,9 +43,11 @@ var openFile = function(event) {
 		
 var canvas= document.getElementById("canvas");
 var ctx=canvas.getContext("2d");	
-//ctx.translate(600, 250);
-//ctx.rotate(Math.PI);
-//ctx.translate(-600, -250);
+
+ctx.translate(300,200);
+ctx.scale(1,-1);
+ctx.translate(-300,-200);
+ctx.fillStyle="green";
 
 var bstart= document.getElementById("start");
 bstart.onclick=function (e){
@@ -52,6 +56,10 @@ bstart.onclick=function (e){
 
 function setup(){
 	samples= (audioBuffer.getChannelData(0)).slice(0);
+	var c;
+	for(c=0;c<n;c++){
+		lev[c]=0;
+	}
 	
 	setInterval(function (){
 		var toCopy= samples.slice(index,index+4800);
@@ -62,7 +70,7 @@ function setup(){
 		var c;
 		for(c=0;c<n;c++){
 			var ct={
-				real:toCopy[c],
+				real:toCopy[c*8],
 				imag:0				
 			};
 			data[c]=ct;
@@ -70,17 +78,47 @@ function setup(){
 		
 		var f=FFT(data);
 		
-		var mags=calcularMags(f);
-		
-		limpar();
-		
-		desenharMags(mags);
-		
+		mags=calcularMags(f);
+		lev=updateLevels(mags,lev);
+				
 		index=index+4800;
 	},50);
-	ctx.fillRect(10,10,10,10);
+	
 	play(audioBuffer);
 	
+	setInterval(function(){
+		limpar();
+		lev=animate(lev);
+		desenharMags(lev);
+		
+	},30);
+	
+}
+
+function animate(levels){
+	var out=new Array();
+	var speed=1;
+	var c;
+	for(c=0;c<levels.length;c++){
+		if(levels[c]>=speed){
+			out[c]=levels[c]-speed;		
+		}else{
+			out[c]=0;
+		}
+	}
+	return out;
+}
+function updateLevels(mags,levels){
+	var out=new Array();
+	var c;
+	for(c=0;c<mags.length;c++){
+		if(mags[c]>levels[c]){
+			out[c]=mags[c];
+		}else{
+			out[c]=levels[c];
+		}
+	}
+	return out;
 }
 
 function limpar(){
@@ -88,10 +126,19 @@ function limpar(){
 }
 
 function calcularMags(freq){
+	var groups=[6,6,6,6,8,8,12,12,16,16,16,16,64,64,128,128];
 	var mags=new Array();
-	var c;
-	for(c=0;c<freq.length;c++){
-		mags[c]= Math.sqrt( Math.pow( freq[c].real,2 )+ Math.pow(freq[c].imag ,2 ));
+	
+	var c,d;
+	var off=0;
+	for(d=0;d<groups.length;d++){
+		var aux=0;
+		for(c=0;c<groups[d];c++){
+			aux+= Math.sqrt( Math.pow( freq[off].real,2 )+ Math.pow(freq[off].imag ,2 ));
+			off++;
+		}
+		aux=aux/groups[d];
+		mags[d]=aux;
 	}
 	return mags;
 }
@@ -99,9 +146,9 @@ function calcularMags(freq){
 function desenharMags(mags){
 	
 	var d=0;
-	for(var c=0;c<=600;c++){
+	for(var c=0;c<=16;c++){
 		var magmedia=mags[c];
-		ctx.fillRect(c,100,1,magmedia);
+		ctx.fillRect(c*32+10,10,30,20*magmedia);
 		
 	}
 	
